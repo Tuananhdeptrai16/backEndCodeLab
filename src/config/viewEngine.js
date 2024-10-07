097b5acdfc74772adb41cbfe10f4bb338e79c042
Tuananhdeptrai16 <Tuananhpro1608@gmail.com>
2024-10-07 19:56:49 +0700

    done couser

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,7 +3,6 @@ const express = require("express");
 const configViewEngine = (app) => {
   app.set("views", path.join("./src", "views"));
   app.set("view engine", "ejs");
-  //config static file
   app.use(express.static(path.join("./src", "public")));
 };
 module.exports = configViewEngine;
