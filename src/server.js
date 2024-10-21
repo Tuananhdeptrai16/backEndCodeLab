ab346e903bf21153dbeface824e95a52c988e6e6
Tuananhdeptrai16 <Tuananhpro1608@gmail.com>
2024-10-21 12:51:34 +0700

    listen 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const connection = require("./config/database");
 const express = require("express"); // comon node
-const configViewEngine = require("./config/viewEngine");
 const apiRoutes = require("./routes/apiRoute");
 // const firebaseRoute = require("./routes/firebase_route");
 const cors = require("cors");
@@ -10,7 +9,6 @@ const app = express(); // khai baos
 const port = process.env.PORT || 8888; // Nếu PORT không được thiết lập, sử dụng cổng 8888
 const hostname = process.env.HOST_NAME || "0.0.0.0"; // Lắng nghe trên tất cả các địa chỉ IP
 //config view engine
-configViewEngine(app);
 app.use(express.json()); // for json
 app.use(express.urlencoded({ extended: true }));
 //test connection
@@ -21,10 +19,20 @@ app.use(
 );
 app.options("*", cors());
 
+// (async () => {
+//   try {
+//     await connection();
+//     app.listen(port, hostname, () => {
+//       console.log(`Sever backEnd of CodeLab on port ${port}`);
+//     });
+//   } catch (error) {
+//     console.log(">>>>Error to connect to db", error);
+//   }
+// })();
 (async () => {
   try {
     await connection();
-    app.listen(port, hostname, () => {
+    app.listen(port, "0.0.0.0", () => {
       console.log(`Sever backEnd of CodeLab on port ${port}`);
     });
   } catch (error) {
